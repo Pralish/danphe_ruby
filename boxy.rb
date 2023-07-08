027fe78c42e22c4a4300380c4675773e218639f0
Pralish Kayastha <pralishkayastha@gmail.com>
2023-07-08 09:31:16 +0545

    refactor boxy

--- boxy.rb
+++ boxy.rb
@@ -1,28 +1,31 @@
 # Write a method - let's call it boxy(n) - which produces output in STDOUT as shown below.
 # 
-# --  --  --  --  --  --  --  --  --  --  --
-# ------------------------------------------
-# | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 |
-# ------------------------------------------
-# --  --  --  --  --  --  --  --  --  --  --
+# ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ 
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# ┃ 1 ┃ 2 ┃ 3 ┃ 4 ┃ 5 ┃ 6 ┃ 7 ┃ 8 ┃ 9 ┃ 10 ┃
+# ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+# ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ 
+
+HORIZONTAL_LINE = 0x2501.chr('UTF-8').freeze
+VERTICAL_LINE = 0x2503.chr('UTF-8').freeze
 
 def boxy(n)
   digit_length = n.to_s.length
-  border_solid = '----' * n  + '-' * digit_length
+  border_solid = HORIZONTAL_LINE * n * 4  + HORIZONTAL_LINE * digit_length
 
   # box with single digit requires 4 '-'s, double digit requires 5 '-'s, and so on.. 
   # this logic with compensate the missing '-'s
   if digit_length > 1
     1.upto(digit_length - 1) do |l|
-      border_solid += '-' * (n - 10.pow(l))
+      border_solid += HORIZONTAL_LINE * (n - 10.pow(l))
     end
   end
 
-  border_dashed = border_solid.gsub('--', '- ')
+  border_dashed = border_solid.gsub(HORIZONTAL_LINE * 2, "#{HORIZONTAL_LINE} ")
 
-  str = "|"
+  str = VERTICAL_LINE
   1.upto(n) do |i|
-    str += " #{i} |"
+    str += " #{i} #{VERTICAL_LINE}"
   end
 
   puts border_dashed
